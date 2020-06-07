40691e5bb81a380d6e7f011a1dba27321bc79419
Maxim Frolov <buterbrodhs@gmail.com>
2020-06-07 17:24:54 +0400

    create project

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# online-store-vue
-Интернет магазин на Vue.js
+# Интерне магазин на Vue.js
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/data/products.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/ratio.jpg',
+  },
+  {
+    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    price: 5660,
+    image: '/img/ratio.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
+    price: 21790,
+    image: '/img/ratio.jpg',
+  },
+];
